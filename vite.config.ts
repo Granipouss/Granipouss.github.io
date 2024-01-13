0601e7d96306a02e7fb58790cb1e7bf06a08a4d6
Brendan Daoud <brendan.daoud@ubisoft.com>
2024-01-13 18:04:02 +0100

    fix: Add 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, '404.html'),
+      },
+    },
   },
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
