24513c397359811f2931d344df9e1433f8c057cd
Brendan Daoud <brendan.daoud@ubisoft.com>
2024-01-13 12:04:01 +0100

    fix: No dynamic imports

--- docs/assets/HomeView-WR4Nxe5B.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-x3n3nnut.js";const r={};function c(t,_){return" HOME "}const o=e(r,[["render",c]]);export{o as default};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeViewVue from '@/views/Home/HomeView.vue'
+import ResumeViewVue from '@/views/Resume/ResumeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: () => '/about'
+      redirect: () => '/about',
     },
     {
       path: '/home',
       name: 'home',
-      component: () => import('../views/Home/HomeView.vue')
+      component: HomeViewVue,
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/Resume/ResumeView.vue')
-    }
-  ]
+      component: ResumeViewVue,
+    },
+  ],
 })
 
 export default router
